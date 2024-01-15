d43816276714f374528e5c6623231b3ea6e05986
Alejandro <adbadthbadn@gmail.com>
2024-01-15 04:01:13 -0300

    create new todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -4,6 +4,7 @@ import { renderTodos } from './use-cases';
 
 const elementIDs = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 }
 
 export const App = (elementId) => {
@@ -13,11 +14,26 @@ export const App = (elementId) => {
         renderTodos( elementIDs.TodoList, todos );
     }
 
-
+    //Cuando la funcion app se llama
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
         displayTodos();
     })();
+
+    //Referencias HTML
+    const newDescriptionInput = document.querySelector( elementIDs.NewTodoInput );
+
+
+    //Listeners
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if( event.keyCode !== 13 ) return;
+        if( event.target.value.trim().length === 0 ) return;
+
+        todoStore.addTodo( event.target.value );
+        displayTodos();
+        event.target.value = '';
+
+    })
 }
