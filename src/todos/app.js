c28cdc3b23fe4aeda8957b81039fbf27799bc144
Alejandro <adbadthbadn@gmail.com>
2024-01-21 19:31:10 -0300

    filter switch added

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,11 +1,12 @@
 import html from './app.html?raw';
-import todoStore from '../store/todo.store';
+import todoStore, { Filters } from '../store/todo.store';
 import { renderTodos } from './use-cases';
 
 const elementIDs = {
     ClearCompleted: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
+    TodoFilters : '.filtro'
 }
 
 export const App = (elementId) => {
@@ -27,6 +28,7 @@ export const App = (elementId) => {
     const newDescriptionInput = document.querySelector( elementIDs.NewTodoInput );
     const TodoListUL = document.querySelector( elementIDs.TodoList );
     const ClearCompletedButton = document.querySelector( elementIDs.ClearCompleted );
+    const filtersLIs = document.querySelectorAll( elementIDs.TodoFilters );
 
 
     //Listeners
@@ -58,4 +60,24 @@ export const App = (elementId) => {
         todoStore.deleteCompleted();
         displayTodos();
     })
+
+    filtersLIs.forEach( element => {
+        element.addEventListener('click', (element) => {
+            filtersLIs.forEach( el => el.classList.remove('selected') );
+            element.target.classList.add('selected');
+            switch( element.target.text ) {
+                case 'Todos':
+                    todoStore.setFilter( Filters.All )
+                break;
+                case 'Pendientes':
+                    todoStore.setFilter( Filters.Pending )
+                break;
+                case 'Completados':
+                    todoStore.setFilter( Filters.Completed )
+                break;
+            }
+            displayTodos();
+        })
+    } )
+
 }
